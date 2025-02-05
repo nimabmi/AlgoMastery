7d9b398356ee55d93f90fce6342bb424b3caa63b
nimabmi <79307158+nimabmi@users.noreply.github.com>
2025-02-05 21:41:45 +0100

    Update jumpgame_v1.py

--- String/55_Jump Game/jumpgame_v1.py
+++ String/55_Jump Game/jumpgame_v1.py
@@ -1,15 +1,15 @@
-#class Solution:
-#   def canJump(self, nums: list[int]) -> bool:
-nums = [2,3,1,1,4]
-n = len(nums)
-goal = n - 1
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        goal = n - 1
 
-for i in range(n - 2, -1, -1):
-    if i + nums[i] >= goal:
-        goal = i
+        for i in range(n - 2, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
 
 # If the goal is the first index, return True
-if goal == 0:
-    print(True)
-else:
-    print(False)
+        if goal == 0:
+            return True
+        else:
+            return False
+    
